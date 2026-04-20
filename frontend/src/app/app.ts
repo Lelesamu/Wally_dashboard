bac3dec41403fdc3fb3e0dafd2dd458c7ff782a7
Lelesamu <samuele.bellani@studenti.unimi.it>
2026-04-20 14:09:57 +0200

    - Riconfigurazione del progetto - Documentazione

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,15 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Button } from 'primeng/button';
+import { PrimeNG } from 'primeng/config';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Button],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+
+  protected readonly title = signal('Frontend');
+}
