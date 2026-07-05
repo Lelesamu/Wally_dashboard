b2274928e0c29fccce42914a00bfc5f663fabc8c
Lelesamu <samuele.bellani@studenti.unimi.it>
2026-07-05 23:49:52 +0200

    Setup TypeOrm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,7 +17,7 @@ import { UsersModule } from './users/users.module';
         username: configService.get('POSTGRES_USER'),
         password: configService.get('POSTGRES_PASSWORD'),
         database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true, // Be cautious about using synchronize in production
       }),
       inject: [ConfigService],
