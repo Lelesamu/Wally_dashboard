75300ba973280ca501430a4a48780f89ea1105f1
Lelesamu <samuele.bellani@studenti.unimi.it>
2026-08-06 15:55:03 +0200

    Start maschera di login

--- frontend/src/app/component/login-component/login-component.ts
+++ frontend/src/app/component/login-component/login-component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  Button } from 'primeng/button';
-import {  Divider } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-component',
-  imports: [Button, Divider],
+  imports: [FormsModule, CommonModule, InputTextModule],
   standalone: true,
   templateUrl: './login-component.html',
   styleUrl: './login-component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public username: string = '';
+  public password: string = '';
+
+  public constructor() {}
+  
+  login(): void {
+    console.log(this.username);
+  }
+}
