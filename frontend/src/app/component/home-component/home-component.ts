1b863e61be24425d49a72edbccc1e37a56e87b50
Lelesamu <samuele.bellani@studenti.unimi.it>
2026-08-24 14:00:23 +0200

    Start drawer menu principale

--- frontend/src/app/component/home-component/home-component.ts
+++ frontend/src/app/component/home-component/home-component.ts
@@ -2,25 +2,7 @@ import { Component, signal } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { DrawerModule } from 'primeng/drawer';
 import { ButtonModule } from 'primeng/button';
-import {
-  Sidebar,
-  SidebarLayout,
-  SidebarAside,
-  SidebarPanel,
-  SidebarHeader,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupLabel,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarMain,
-  SidebarTrigger,
-  SidebarSpacer,
-  SidebarBackdrop,
-} from 'primeng/sidebar';
+
 
 //TODO: implentare il drawer laterale. La sidebar in primeng non funziona, è necessario il drawer o usare primeng di versione superiore.
 @Component({
@@ -34,17 +16,5 @@ import {
   styleUrl: './home-component.scss',
 })
 export class HomeComponent {
-
-  isMobile = signal(false);
-  
-  constructor() {
-      if (typeof window === 'undefined') return;
-      const mql = window.matchMedia('(max-width: 1023px)');
-      this.isMobile.set(mql.matches);
-      mql.addEventListener('change', (e) => this.isMobile.set(e.matches));
-  }
-
-  ngOnInit() {
-    console.log('here')
-  }
+  visible: boolean = false;
 }
